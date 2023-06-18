6715c420820b9b9736c51b41669a778fcdb67ee6
MahmoudAbid1 <youssef.abid@strategix.eu>
2023-06-18 13:41:25 +0200

    Version4

---
+++ officestatistic.h
@@ -0,0 +1,14 @@
+#ifndef OFFICESTATISTIC_H
+#define OFFICESTATISTIC_H
+#include<QString>
+#include <QChartView>
+#include<QtCharts>
+#include<QChart>
+
+class OfficeStatistic
+{
+public:
+  QChart *statistic();
+};
+
+#endif // OFFICESTATISTIC_H
